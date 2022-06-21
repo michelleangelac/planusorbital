6cc994ec711e2ab21c07d6dd149a0d3d60095ee2
Gwyneth Guo Bai Ling <gwynethguo@gmail.com>
2022-06-21 13:46:07 +0700

    Change Protected Route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navigate, Route, Outlet, useLocation } from "react-router-dom";
-import {firebaseAuth} from "../hooks/useAuth";
+import {firebaseAuth, useAuth} from "../hooks/useAuth";
 
 export const ProtectedRoute = ({children}) =>  {
-    var user = firebaseAuth.currentUser;
-    const location = useLocation();
-    if(user) {
-        return children;
-    }
-    return <Navigate to = "/login"/>;
+    var user = useAuth();
+    return typeof user === 'null' ? (
+        <h1>Loading.....</h1>
+      ) : user ? (
+        children
+      ) : (
+        <Navigate to = "/login"/>
+      );
 }
