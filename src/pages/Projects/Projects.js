927c3b3ee415d906c18bc6550fb17e966fe0e27b
Gwyneth Guo Bai Ling <gwynethguo@gmail.com>
2022-07-25 14:03:20 +0700

    Projects Add Members, Modify, Delete (-)Tasks & Progress

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -9,6 +9,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import Tabs from "../../components/Sidebar/Tabs";
 import Project from "./Project";
 import PopupSch from "../Schedules/Popup";
+import MemberList from "./Member";
 
 import "./Projects.css"
 import "@fontsource/inter";
@@ -17,11 +18,10 @@ import { db, firebaseAuth, useAuth } from "../../hooks/useAuth";
 import { doc, setDoc, collection, query, where, getDocs, addDoc, getDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { SettingsSystemDaydreamRounded } from "@mui/icons-material";
+import { Email, SettingsSystemDaydreamRounded } from "@mui/icons-material";
 
 const initialState = {
   name: "",
-  groupName: "",
   members: [],
   startDate: new Date(),
   endDate: new Date(),
@@ -40,6 +40,17 @@ async function getProjects(user) {
   }
 }
 
+async function getUser(email) {
+  console.log("email", email);
+  const q = query(collection(db, "profile"), where("email", "==", email));
+  try {
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 function Projects() {
   const navigate = useNavigate();
 
@@ -48,10 +59,35 @@ function Projects() {
   const [isOpen, setIsOpen] = useState(false);
 
   const [values2, setValues2] = useState(initialState);
+  
+  const [userExists, setUserExists] = useState(false);
 
-  function togglePopup() {
+  const [isSameUser, setIsSameUser] = useState(false);
+  
+  const [members, setMembers] = useState([]);
+
+  const [currentMemberAdded, setCurrentMemberAdded] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  const [add, setAdd] = useState(false);
+
+  const [num, setNum] = useState(0);
+
+  function togglePopupOpen() {
+    var user = firebaseAuth.currentUser;
+    setValues2(initialState);
+    if (!members.includes(user.email)) {
+      setMembers(prev => [...prev, user.email]);
+    }
+    setIsOpen(!isOpen);
+  }
+
+  function togglePopupClose() {
     setValues2(initialState);
     setIsOpen(!isOpen);
+    setMembers([]);
+    setMessage("");
   }
 
   const [startDate, setStartDate] = useState(new Date());
@@ -65,22 +101,75 @@ function Projects() {
     //setOpenSb(true);
     var user = firebaseAuth.currentUser;
     //console.log(user);
-    addDoc(collection(db, "projects"), { user: user.email, name: values2.name, groupName: values2.groupName, members: values2.members, startDate: startDate, endDate: endDate, progress: 0 });
+    addDoc(collection(db, "projects"), { user: user.email, name: values2.name, members: members, startDate: startDate, endDate: endDate, progress: 0 });
     setProjects([]);
-      getProjects(user).then(userData => userData.forEach(x => setProjects(prev => [...prev, x.id]))).catch(err => console.log(err));
-    togglePopup();
+    getProjects(user).then(userData => userData.forEach(x => setProjects(prev => [...prev, x.id]))).catch(err => console.log(err));
+    togglePopupClose();
   }
+
+  function handleAdd() {
+    console.log(currentMemberAdded.toString());
+    if (members.includes(currentMemberAdded)) {
+      console.log("bener");
+      setIsSameUser(true);
+      // console.log(isSameUser);
+    }
+    else {
+      setIsSameUser(false);
+      getUser(currentMemberAdded).then(userData => setNum(userData.length));
+      getUser(currentMemberAdded.toString()).then(userData => userData.forEach(x => {
+        // setNum(prev => prev+1);
+        // console.log("ada dlm", ada);
+        // handle kalo mem  ber udah ada di array
+        // handle kalo user add user sendiri
+        setMembers(prev => [...prev, x.get("email")]);
+      }));
+    }
+    // console.log("ada", ada);
+    // setCurrentMemberAdded("");  
+    setAdd(true);
+    // setIsSameUser(false);
+    // setUserExists(false);
+  }
+
+  // function handleAdd1() {
+  //   handleAdd()
+  //   .then(() => ;
+  //   // console.log(isSameUser);
+  //   // setMessage(isSameUser ? "User is already a member!" : (userExists ? "User added successfully!" : "User not found!"));
+  // }
 
   useEffect(() => {
     firebaseAuth.onAuthStateChanged((user) => {
       if (user) {
-        setProjects([]);
-          getProjects(user).then(userData => userData.forEach(x => setProjects(prev => [...prev, x.id]))).catch(err => console.log(err));
+        // setProjects([]);
+        if (projects.length == 0) {
+          getProjects(user)
+          .then(userData => userData.forEach(x => {
+              setProjects(prev => [...prev, x.id]);
+          }))
+          .catch(err => console.log(err));
+        }
+        if (!members.includes(user.email)) {
+          setMembers(prev => [...prev, user.email]);
+        }
+        console.log("members", members);
+        console.log(add);
+        console.log(isSameUser);
+        console.log(num);
+        getUser(currentMemberAdded).then(userData => setNum(userData.length));
+        if (add) {
+          setMessage(isSameUser ? "User is already a member!" : ( num > 0 ? "User added successfully!" : "User not found!"));
+          setCurrentMemberAdded("");
+          setNum(0);
+          setIsSameUser(false);
+          setAdd(false);
+        }
       } else {
         navigate("/login");
       }
     });
-  }, [])
+  }, [add, currentMemberAdded])
 
   console.log(projects);
 
@@ -98,7 +187,7 @@ function Projects() {
         </div>
         <Button className="add-project"
           startIcon={<IoIcons.IoIosAdd />}
-          onClick={togglePopup}>
+          onClick={togglePopupOpen}>
           Add project
         </Button>
         {isOpen && <PopupSch
@@ -113,16 +202,22 @@ function Projects() {
                     <input className="prj-name" type="text" value={values2.name} onChange={handleChange("name")}></input>
                   </div>
                   <div style={{ textAlign: 'left', margin: '2% 0  0 10%', fontSize: '1.15em', fontFamily: 'Inter', fontWeight: 600 }}>
-                    <label>Group Name</label>
-                  </div>
-                  <div>
-                    <input className="prj-name" type="text" value={values2.groupName} onChange={handleChange("groupName")}></input>
-                  </div>
-                  <div style={{ textAlign: 'left', margin: '2% 0  0 10%', fontSize: '1.15em', fontFamily: 'Inter', fontWeight: 600 }}>
                     <label>Members</label>
                   </div>
                   <div>
-                    <input className="prj-members" type="text" value={values2.members} onChange={handleChange("members")}></input>
+                    <input className="prj-members" type="text" value={currentMemberAdded} onChange={(event) => setCurrentMemberAdded(event.target.value)}></input>
+                    <div className="right" style={{display: 'inline-flex'}}>
+                      <Button
+                        variant="contained"
+                        onClick={() => handleAdd()}
+                        style={{ margin: '3% 0 3% 0', width: '75%', backgroundColor: '#000000', height: '6.5vh'}}>
+                        Add
+                      </Button>
+                    </div>
+                    <div>{message}</div>
+                    <div style={{ marginLeft: '5%' }}>
+                    {members.map(x => <MemberList email={x} setMembers = {setMembers}/>)}
+                    </div>
                   </div>
                   <div style={{ textAlign: 'left', margin: '2% 0  0 10%', fontSize: '1.15em', fontFamily: 'Inter', fontWeight: 600 }}>
                     <label>Start Date</label>
@@ -153,7 +248,7 @@ function Projects() {
                 </Button>
               </>
             }
-            handleClose={togglePopup}
+            handleClose={togglePopupClose}
           />}
       </div>
       <div className="completed-prj">
