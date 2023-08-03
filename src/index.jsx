6ebe1cf94c20c3116e4131c0c050b10faaee5b10
gwynethguo <e0905362@u.nus.edu>
2023-08-03 22:25:23 +0800

    Import index.css to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ProvideAuth } from "./hooks/useAuth";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
