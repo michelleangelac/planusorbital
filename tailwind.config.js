8fd47740a9f294a962ebf5432eced3541f19ca1c
gwynethguo <e0905362@u.nus.edu>
2023-08-04 11:24:33 +0800

    Add screens and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    // Screen settings source: https://tailwindcss.com/docs/screens
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
       purple: "#d0d4eb",
       pink: "#e5d0e3",
@@ -12,6 +29,7 @@ module.exports = {
       black: "#000000",
       white: "#ffffff",
       grey: "#c3c3c3",
+      hyperlink: "#5062ad",
     },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
