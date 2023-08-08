8ac0e22c51c568ede5abe48608aeaa209c5440df
gwynethguo <e0905362@u.nus.edu>
2023-08-08 18:58:44 +0800

    Implement firebase auths

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,9 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 export const config = {
-    apiKey: "AIzaSyBnyldLcuB5k3rUhRNLqujJ9ZO5xMuqLGE",
-    authDomain: "planus-c5393.firebaseapp.com",
-    projectId: "planus-c5393",
-    storageBucket: "planus-c5393.appspot.com",
-    messagingSenderId: "1037326455331",
-    appId: "1:1037326455331:web:8d757ad5d7de10ea5e9796",
-    measurementId: "G-KJLK7PEYZX"
-  };
+  apiKey: "AIzaSyBnyldLcuB5k3rUhRNLqujJ9ZO5xMuqLGE",
+  authDomain: "planus-c5393.firebaseapp.com",
+  projectId: "planus-c5393",
+  storageBucket: "planus-c5393.appspot.com",
+  messagingSenderId: "1037326455331",
+  appId: "1:1037326455331:web:8d757ad5d7de10ea5e9796",
+  measurementId: "G-KJLK7PEYZX",
+};
+
+const app = initializeApp(config);
+export const auth = getAuth(app);
+
+export default app;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Button, styled } from "@material-ui/core";
+import { auth } from "../config/firebase";
 import {
-  InputLabel,
-  FormControl,
-  Avatar,
-  IconButton,
-  InputAdornment,
-  Snackbar,
-  Alert,
-} from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-
-// import "./LoginPage.css";
-import "@fontsource/inter";
-import BootstrapInput from "../components/BootstrapInput";
-
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { Button, styled } from "@material-ui/core";
 import { firebaseAuth, useAuth, db } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import {
@@ -103,6 +95,7 @@ const PasswordInput = ({
 
 function PageLogin() {
   const navigate = useNavigate();
+  const googleAuthProvider = new GoogleAuthProvider();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -119,107 +112,30 @@ function PageLogin() {
   const togglePassword = () => {
     setShowPassword(!showPassword);
   };
-  //Password visibility
-  const [values, setValues] = React.useState({
-    username: "",
-    name: "",
-    email: "",
-    password: "",
-    faculty: "",
-    showPassword: false,
-  });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-  };
-
-  const [openSb, setOpenSb] = useState(false);
-  const handleCloseSb = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpenSb(false);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    signin(values.email, values.password);
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      console.log(user);
-      if (user) {
-        //setOpenSb(true);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((resp) => {
         navigate("/dashboard");
-      } else {
-        navigate("#");
-      }
-    });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const handleSubmitGoogle = async (event) => {
+  const handleLoginWithGoogle = async (event) => {
     event.preventDefault();
-    const result = await signInWithGoogle();
-    const { isNewUser } = getAdditionalUserInfo(result);
-    var profilePic = getAdditionalUserInfo(result).profile.picture;
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        if (isNewUser) {
-          setDoc(doc(db, "profile", user.email), {
-            username: "test",
-            name: user.displayName,
-            email: user.email,
-            password: values.password,
-            faculty: values.faculty,
-            profile: profilePic,
-          });
-        }
-        //setOpenSb(true);
+    console.log("handleGoogle");
+    await signInWithPopup(auth, googleAuthProvider)
+      .then((resp) => {
+        console.log("HERE", "");
         navigate("/dashboard");
-      } else {
-        navigate("#");
-      }
-    });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-
-  function handleOpenSb() {
-    () => setOpenSb(true);
-  }
-
-  const { signInWithGoogle } = useAuth() || {};
-  const { signin } = useAuth() || {};
-
-  const BootstrapButton = styled(Button)({
-    boxShadow: "none",
-    textTransform: "none",
-    fontSize: "1em",
-    lineHeight: 2,
-    width: "50vh",
-    textAlign: "center",
-    backgroundColor: "#FFFFFF",
-    color: "#736E6E",
-    fontWeight: "600",
-    fontFamily: ["Inter"].join(","),
-    "&:hover": {
-      backgroundColor: "#EEEBEB",
-      boxShadow: "none",
-    },
-    "&:active": {
-      boxShadow: "none",
-      backgroundColor: "#FFFFFF",
-    },
-  });
 
   return (
     <div className="flex flex-col md:flex-row min-w-screen min-h-screen">
@@ -238,7 +154,7 @@ function PageLogin() {
         <div className="text-[20px] md:text-[29px] font-extrabold mb-8 text-center">
           Login to your account.
         </div>
-        <form className="input-form">
+        <form className="input-form" onSubmit={handleLogin}>
           <Input
             label="Email"
             id="email"
@@ -268,138 +184,32 @@ function PageLogin() {
           >
             Login
           </button>
-          <div className="flex justify-stretch items-center">
-            <div className="border border-grey/50 h-0 w-[45%]" />
-            <div className="text-grey text-[13px] mx-3">or</div>
-            <div className="border border-grey/50 h-0 w-[45%]" />
-          </div>
-          <button
-            className="border border-grey w-full font-semibold text-[16px] py-2 rounded-xl my-7"
-            type="submit"
-          >
-            <div className="flex justify-center items-center">
-              <img className="w-7 mx-2" src={google} />
-              Continue with Google
-            </div>
-          </button>
-          <div className="flex justify-center">
-            <div className="text-[13px]">
-              Not registered yet?{" "}
-              <a className="font-bold text-hyperlink" href="#">
-                Create an account.
-              </a>
-            </div>
-          </div>
         </form>
+        <div className="flex justify-stretch items-center">
+          <div className="border border-grey/50 h-0 w-[45%]" />
+          <div className="text-grey text-[13px] mx-3">or</div>
+          <div className="border border-grey/50 h-0 w-[45%]" />
+        </div>
+        <button
+          className="border border-grey w-full font-semibold text-[16px] py-2 rounded-xl my-7"
+          type="submit"
+          onClick={handleLoginWithGoogle}
+        >
+          <div className="flex justify-center items-center">
+            <img className="w-7 mx-2" src={google} />
+            Continue with Google
+          </div>
+        </button>
+        <div className="flex justify-center">
+          <div className="text-[13px]">
+            Not registered yet?{" "}
+            <a className="font-bold text-hyperlink" href="#">
+              Create an account.
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-    //   <div className="container-log">
-    //     <div className="blue-bg"></div>
-    //     <div className="content-log">
-    //       <div className="title-log">Login to Your Account</div>
-    //       <div>
-    //         <BootstrapButton
-    //           className="google-button"
-    //           variant="outlined"
-    //           startIcon={
-    //             <Avatar
-    //               sx={{ width: 20, height: 20 }}
-    //               src={
-    //                 "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
-    //               }
-    //             />
-    //           }
-    //           onClick={handleSubmitGoogle}
-    //         >
-    //           Continue with Google
-    //         </BootstrapButton>
-    //       </div>
-    //       <div className="hl">
-    //         <span className="hl span">or</span>
-    //       </div>
-    //       <FormControl variant="standard" style={{ margin: "3% 0 3% 0" }}>
-    //         <InputLabel shrink htmlFor="bootstrap-input">
-    //           Email
-    //         </InputLabel>
-    //         <BootstrapInput
-    //           id="email-input"
-    //           style={{ width: "45vh" }}
-    //           value={values.email}
-    //           onChange={handleChange("email")}
-    //         />
-    //       </FormControl>
-    //       <FormControl variant="standard" style={{ margin: "3% 0 0.5% 7.5%" }}>
-    //         <InputLabel shrink htmlFor="bootstrap-input">
-    //           Password
-    //         </InputLabel>
-    //         <BootstrapInput
-    //           id="password-input"
-    //           style={{ width: "51.25vh" }}
-    //           type={values.showPassword ? "text" : "password"}
-    //           value={values.password}
-    //           onChange={handleChange("password")}
-    //           endAdornment={
-    //             <InputAdornment position="end">
-    //               <IconButton
-    //                 aria-label="toggle password visibility"
-    //                 onClick={handleClickShowPassword}
-    //                 onMouseDown={handleMouseDownPassword}
-    //                 edge="end"
-    //               >
-    //                 {values.showPassword ? <VisibilityOff /> : <Visibility />}
-    //               </IconButton>
-    //             </InputAdornment>
-    //           }
-    //         />
-    //       </FormControl>
-    //       <p
-    //         style={{
-    //           fontSize: "0.75em",
-    //           paddingLeft: "30%",
-    //           fontWeight: "600",
-    //           fontFamily: "Inter",
-    //         }}
-    //       >
-    //         <a href="/resetpassword" className="resetpw-link">
-    //           Forget your password?
-    //         </a>
-    //       </p>
-    //       <Button
-    //         style={{
-    //           width: "50vh",
-    //           lineHeight: 2,
-    //           fontSize: "1em",
-    //           margin: "4% 0 0.5% 0",
-    //           backgroundColor: "#5062AD",
-    //           color: "#FFFFFF",
-    //         }}
-    //         variant="contained"
-    //         onClick={handleSubmit}
-    //       >
-    //         Login
-    //       </Button>
-    //       <Snackbar open={openSb} autoHideDuration={6000} onClose={handleCloseSb}>
-    //         <Alert
-    //           onClose={handleCloseSb}
-    //           severity="success"
-    //           sx={{ width: "100%" }}
-    //         >
-    //           Logged In
-    //         </Alert>
-    //       </Snackbar>
-    //       <p style={{ fontSize: "0.75em", fontFamily: "Inter" }}>
-    //         Not registered yet?{" "}
-    //         <a href="/signup" className="signup-link">
-    //           Create an account
-    //         </a>
-    //       </p>
-    //       <div
-    //         style={{ fontSize: "0.8em", fontFamily: "Inter", marginTop: "5%" }}
-    //       >
-    //         PlaNUS ©2022
-    //       </div>
-    //     </div>
-    //   </div>
   );
 }
 
